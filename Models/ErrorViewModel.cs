61b85d193efc171d5d40982aa0d9638419a404c3
Htet <htetmonoo43@gmail.com>
2026-01-01 12:43:58 +0630

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManageTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
